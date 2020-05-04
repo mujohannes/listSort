04295372177f16147621d221b8a36cb013dd161b
johannes muljana <johannes.muljana@tafensw.edu.au>
2020-05-04 13:43:59 +1000

    item added to perishables

--- src/script.js
+++ src/script.js
@@ -4,29 +4,109 @@ async function loadData(url) {
   return data;
 }
 
+function removeFromList( list, id ) {
+  list.forEach((item, index ) => {
+    if(item.id == id ) {
+      list.splice( index, 1 );
+    }
+  });
+}
+
+function renderMaster(list, element ) {
+  element.innerHTML = '';
+  list.forEach( (item) => {
+    let id = item.id;
+    let name = item.name;
+    let unit = item.unit;
+    let category = item.category;
+    let listItem = 
+    `<li data-id="${id}">
+      ${name}
+      <div class="buttons">
+        <button type="button" 
+        class="button" 
+        data-action="perishable"
+        data-id="${id}"
+        data-name="${name}"
+        data-unit="${unit}"
+        data-category="${category}"
+        >
+          Perishable
+        </button>
+        <button type="button" class="button" 
+        data-action="non-perishable"
+        data-id="${id}"
+        data-name="${name}"
+        data-unit="${unit}"
+        data-category="${category}"
+        >
+          Non-Perishable
+        </button>
+      </div>
+    </li>`;
+    element.insertAdjacentHTML('beforeend', listItem );
+  })
+}
+
+function renderPerishables( list, element ) {
+  element.innerHTML = '';
+  list.forEach( (item) => {
+    let id = item.id;
+    let name = item.name;
+    let unit = item.unit;
+    let category = item.category;
+    let listItem = 
+    `<li data-id="${id}">
+      ${name}
+      <div class="buttons">
+        <button type="button" 
+        class="button" 
+        data-action="perishable"
+        data-id="${id}"
+        data-name="${name}"
+        data-unit="${unit}"
+        data-category="${category}"
+        >
+          Remove
+        </button>
+      </div>
+    </li>`;
+    element.insertAdjacentHTML('beforeend', listItem );
+  })
+}
+
 let masterList = new Array();
+let perishablesList = new Array();
+let nonPerishablesList = new Array();
+
 window.addEventListener('load', () => {
+  const masterDisplay = document.querySelector('#master');
+  const perishableDisplay = document.querySelector('#perishables');
   // load the data
   const dataFile = 'data.json';
-  // let response = await fetch(dataFile);
-  // if( response.ok ) {
-  //   let data = await response.json();
-  //   console.log( response );
-  // }
-  // second try
   loadData( dataFile )
   .then( (data) => { 
-    const masterDisplay = document.querySelector('#master');
-    // display the items in master
-    data.forEach( (item) => {
-      let id = item.id;
-      let name = item.name;
-      masterList.push( item );
-      let listItem = `<li data-id="${id}">
-                      ${name}
-                      </li>`;
-      masterDisplay.insertAdjacentHTML('beforeend', listItem );
+    data.forEach((item) => {
+      masterList.push(item)
     })
+    renderMaster(masterList, masterDisplay )
   })
+  // listen for click in master
+  masterDisplay.addEventListener('click', (event) => {
+    const action = event.target.getAttribute('data-action');
+    const id = event.target.getAttribute('data-id');
+    const name = event.target.getAttribute('data-name');
+    const unit = event.target.getAttribute('data-unit');
+    const category = event.target.getAttribute('data-category');
+    if( action == 'perishable' ) {
+      const item = {id: id, name: name, unit: unit, category: category }
+      perishablesList.push( item );
+      removeFromList( masterList, id );
+      renderPerishables( perishablesList, perishableDisplay );
+      renderMaster( masterList, masterDisplay );
+    }
+  })
+
+
 })
 
